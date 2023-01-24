2416420422107a9376abd52a91cb2209125a524a
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2023-01-24 14:50:21 +0100

    Task 21: different strategie to reset the form

--- advanced-angular-forms/src/app/pages/template-forms-page/template-forms-page.component.ts
+++ advanced-angular-forms/src/app/pages/template-forms-page/template-forms-page.component.ts
@@ -2,7 +2,13 @@ import { UniqueUsernameDirective } from './../../directives/validators/unique-us
 import { BannedWordsDirective } from './../../directives/validators/banned-words.directive';
 import { PasswordShouldMatchDirective } from './../../directives/validators/password-should-match.directive';
 import { UserInfo } from './../../model/user-info';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 
@@ -24,7 +30,7 @@ import { FormsModule, NgForm } from '@angular/forms';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TemplateFormsPageComponent implements OnInit {
+export class TemplateFormsPageComponent implements OnInit, AfterViewInit {
   public bannedWords = ['admin', 'administrator', 'super_admin', 'moderator'];
   userInfo: UserInfo = {
     firstName: 'Ouakala',
@@ -41,9 +47,20 @@ export class TemplateFormsPageComponent implements OnInit {
     cofirmPassword: 0,
   };
 
+  @ViewChild(NgForm)
+  public formDir!: NgForm;
+
+  public initFormValues: unknown;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    queueMicrotask(() => {
+      this.initFormValues = this.formDir.value;
+    });
+  }
 
   get years() {
     const now = new Date().getUTCFullYear();
@@ -61,7 +78,14 @@ export class TemplateFormsPageComponent implements OnInit {
     console.log('ngForm' + ngForm);
     console.log('event' + event);
     console.log('Form submitted!');
-    ngForm.reset();
-    console.log('Form reset!');
+    // Strategy 1 - Reset form values, validation statuses, making controls untouched, pristine, etc
+    this.formDir.resetForm(this.formDir);
+    this.initFormValues = this.formDir.value;
+    // Strategy 2 - Reset only control statuses but not values.
+  }
+
+  onReset(ngForm: NgForm, event: Event) {
+    event.preventDefault();
+    this.formDir.resetForm(this.initFormValues);
   }
 }
