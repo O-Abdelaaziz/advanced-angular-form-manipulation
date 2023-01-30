cbeace9a0103e2f63efabd4375b2fc9075fd0ddc
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2023-01-30 13:38:37 +0100

    Task 28: standalone control in the reactive forms

--- advanced-angular-forms/src/app/core/header/header.component.ts
+++ advanced-angular-forms/src/app/core/header/header.component.ts
@@ -1,19 +1,30 @@
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit {
   public searchString = '';
+  public reactiveSearchString = new FormControl('');
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.reactiveSearchString.valueChanges
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe(console.log);
+  }
+
+  findSomething(search: string) {
+    console.log(search);
+  }
 }
