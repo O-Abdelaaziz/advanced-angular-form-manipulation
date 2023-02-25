8305e6fd0372122d16d0a1852ad36e80014ee4a1
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2023-02-25 09:29:43 +0100

    Task 49: adding animation to the drop down panel

--- projects/custom-form-controls/src/lib/select/select.component.ts
+++ projects/custom-form-controls/src/lib/select/select.component.ts
@@ -1,9 +1,39 @@
-import { Component, OnInit, Input, HostListener } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  AnimationEvent,
+} from '@angular/animations';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  HostListener,
+  EventEmitter,
+} from '@angular/core';
+
+// There is an alias for 'void => *' is ':enter'
+// There is an alias for '* => void' is ':leave'
 
 @Component({
   selector: 'lib-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
+  animations: [
+    trigger('dropDown', [
+      state('void', style({ transform: 'scaleY(0)', opacity: 0 })),
+      state('*', style({ transform: 'scaleY(1)', opacity: 1 })),
+      transition(':enter', [
+        animate('320ms cubic-bezier(0, 1, 0.45, 1.34)'),
+      ]),
+      transition(':leave', [
+        animate('420ms cubic-bezier(0.88, -0.7, 0.86, 0.85)'),
+      ]),
+    ]),
+  ],
 })
 export class SelectComponent implements OnInit {
   @Input()
@@ -11,6 +41,12 @@ export class SelectComponent implements OnInit {
 
   @Input()
   public value: string | null = null;
+
+  @Output()
+  public opened = new EventEmitter<void>();
+
+  @Output()
+  public closed = new EventEmitter<void>();
 
   public isOpen: boolean = false;
 
@@ -26,4 +62,14 @@ export class SelectComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  public onPanelAnimationDone({ fromState, toState }: AnimationEvent) {
+    if (fromState === 'void' && toState === null && this.isOpen) {
+      this.opened.emit();
+    }
+
+    if (fromState === null && toState === 'void' && !this.isOpen) {
+      this.closed.emit();
+    }
+  }
 }
