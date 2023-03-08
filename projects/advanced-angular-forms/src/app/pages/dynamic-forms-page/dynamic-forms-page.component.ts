70b54801f8ed2550eabcce673d55eac5eeb08e20
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2023-03-08 09:00:51 +0100

    Task 71: render the form from config

--- projects/advanced-angular-forms/src/app/pages/dynamic-forms-page/dynamic-forms-page.component.ts
+++ projects/advanced-angular-forms/src/app/pages/dynamic-forms-page/dynamic-forms-page.component.ts
@@ -1,12 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, Subject, switchMap } from 'rxjs';
+import { Observable, Subject, switchMap, tap } from 'rxjs';
+import { DynamicFormConfig } from '../../model/dynamic-forms';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-forms-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './dynamic-forms-page.component.html',
   styleUrls: [
     './dynamic-forms-page.component.scss',
@@ -15,17 +17,32 @@ import { Observable, Subject, switchMap } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DynamicFormsPageComponent implements OnInit {
+  public form!: FormGroup;
   protected formLoadingTrigger: Subject<'user' | 'company'> = new Subject<
     'user' | 'company'
   >();
 
-  protected formConfig$!: Observable<object>;
+  protected formConfig$!: Observable<DynamicFormConfig>;
 
   constructor(private _httpClient: HttpClient) {}
 
   ngOnInit(): void {
     this.formConfig$ = this.formLoadingTrigger.pipe(
-      switchMap((config) => this._httpClient.get(`assets/${config}.form.json`))
+      switchMap((config) =>
+        this._httpClient.get<DynamicFormConfig>(`assets/${config}.form.json`)
+      ),
+      tap(({ controls }) => this.buildForm(controls))
     );
+  }
+
+  public buildForm(controls: DynamicFormConfig['controls']): void {
+    this.form = new FormGroup({});
+    Object.keys(controls).forEach((key) => {
+      this.form.addControl(key, new FormControl(controls[key].value));
+    });
+  }
+  public onSubmitForm() {
+    console.log('Form Submited ' + this.form.value);
+    this.form.reset();
   }
 }
